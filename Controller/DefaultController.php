df462ea9b3ece1455359656d40b4b7e4089f1560
hlecorche <hlecorche@e-commit.fr>
2014-06-30 17:23:01 +0200

    Remove unused use statement

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -21,7 +21,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DefaultController extends Controller
